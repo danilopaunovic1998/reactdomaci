e1748a724f0dc2bc966afda3519574fd234ec010
danilopaunovic1998 <57719918+danilopaunovic1998@users.noreply.github.com>
2022-06-18 18:21:51 +0200

    Stilizovanje header-a

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from "../logo.jpg"
+import logo from "../logo.png"
 export default function Header() {
-    
-        
-    
+
+
+
     return (
-        <div>
-            <img src={logo}></img>
-            <Link to= "/"><p>Home</p></Link>
-            <Link to="/watchlist"><p>My Watch List</p></Link>
+        <div class="header">
+            <nav>
+                <img class="logo-image" src={logo}></img>
+                <ul class = "nav-links">
+                    <li>
+                        <Link to="/"><a class="nav-item">Home</a></Link>
+                    </li>
+                    <li>
+                        <Link to="/watchlist"><a class="nav-item">My Watch List</a></Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
