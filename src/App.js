d3c2f53a63a00c276e6c19f1c418968d39d4e6fd
danilopaunovic1998 <57719918+danilopaunovic1998@users.noreply.github.com>
2022-06-18 16:52:59 +0200

    rutiranje stranice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
+import {
+  BrowserRouter as Router,
+  Routes , Route
+} from "react-router-dom";
+import WatchList from './pages/WatchList';
+import Home from './pages/Home';
 function App() {
+  
   return (
     <div className="App">
-     
+        <Router>
+        <Header />
+          <Routes>
+            <Route path={"/watchlist"} element = {<WatchList/>}/>
+            <Route path={"/*"} element = {<Home/>}/>
+          </Routes>
+        </Router>
+
     </div>
   );
 }
